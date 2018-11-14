b08839d1fe0c68510969ce3cf18f9a6eba1fc40b
joeyguo <lgsincy@gmail.com>
2018-11-14 10:21:01 +0800

    bugfix https://github.com/joeyguo/js-beautify-sourcemap/issues/1

--- src/js-beautify-sourcemap.js
+++ src/js-beautify-sourcemap.js
@@ -1,18 +1,17 @@
-import beautify from 'js-beautify';
 import { generator, consumer } from 'js-sourcemap';
+const beautify = require('js-beautify');
 
 export default function jsbs(res, opts={}, loc={}) {
-    var dist = beautify(res, opts),
+    let dist = beautify(res, opts),
         sm = generator(res, dist),
         smConsumer = consumer(sm);
 
-    var line = loc.line,
-        column = loc.column;
-
-    var locRes = line !== undefined && column !== undefined && smConsumer.getGenerated({
+    let {
         line,
-        column
-    }) || {};
+        column,
+    } = loc;
+
+    let locRes = line !== undefined && column !== undefined && smConsumer.getGenerated({ line, column }) || {};
     
     return {
         code: dist,
